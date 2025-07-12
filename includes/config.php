6306ceb1ce3514df59a02818e6376f4c32c57f75
charlesmaronilla <151368880+charlesmaronilla@users.noreply.github.com>
2025-07-12 11:18:56 +0800

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'paymongo_secret_key' => 'sk_test_ULomTEF1pcU2b5XNktuRza9e',
+    'paymongo_public_key' => 'pk_test_7HMZjMgQ1Ct9dkNkQ9AugX99',
+    'domain' => 'http://localhost/Kiosk-System'
+];
